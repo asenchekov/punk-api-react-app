5e86d00c2ce25c8ad1f18bc78f957d44130b5850
Asen Chekov <asenchekov@Users-MacBook-Pro.local>
2020-02-14 15:08:17 +0000

    Updates

---
+++ precache-manifest.7ff6be1e7dba0afae2eb3afa0eaf6602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aeb4c0b364ff8afab9e5ebdfc0c86a4",
+    "url": "/punk-api-react-app/index.html"
+  },
+  {
+    "revision": "4322466b44fb4f75f390",
+    "url": "/punk-api-react-app/static/css/main.11eb29bb.chunk.css"
+  },
+  {
+    "revision": "c21cf3e72bcbdd1cf8ef",
+    "url": "/punk-api-react-app/static/js/2.a8fe371f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/punk-api-react-app/static/js/2.a8fe371f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4322466b44fb4f75f390",
+    "url": "/punk-api-react-app/static/js/main.0a2baaa0.chunk.js"
+  },
+  {
+    "revision": "ad4129d987334154b7ed",
+    "url": "/punk-api-react-app/static/js/runtime-main.5646d0a0.js"
+  }
+]);
